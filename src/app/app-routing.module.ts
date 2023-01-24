0e92fff0f8c57d0fd19e738f6aa4eadb84db9dc1
Nina <nikolina.sikmanovic@synergysuite.com>
2023-01-24 11:26:44 +0100

    Routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {QuotesListComponent} from "./components/quotes-list/quotes-list.component";
+import {QuoteComponent} from "./components/quote/quote.component";
+import {LogInComponent} from "./components/log-in/log-in.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'home', component: HomeComponent},
+  {path:'quotes', component: QuotesListComponent},
+  {path:'quote', component: QuoteComponent},
+  {path:'login', component: LogInComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { QuotesListComponent } from './components/quotes-list/quotes-list.component';
+import { MatButtonModule} from "@angular/material/button";
+import { HomeComponent } from './components/home/home.component';
+import { QuoteComponent } from './components/quote/quote.component';
+import { LogInComponent } from './components/log-in/log-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    QuotesListComponent
+    QuotesListComponent,
+    HomeComponent,
+    QuoteComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatSliderModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
